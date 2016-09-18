099d2053363af2f3af040ffaefc0ca5248aa6266
t-kenji <protect.2501@gmail.com>
2016-09-18 12:02:57 +0000

    Add trac-autocomplete-provider-plugin support

--- trac_emoji/trac_emoji.py
+++ trac_emoji/trac_emoji.py
@@ -6,26 +6,52 @@ from genshi.builder import tag
 
 from trac.core import *
 from trac.wiki.api import IWikiSyntaxProvider
-from trac.web.chrome import ITemplateProvider
+from trac.web.chrome import ITemplateProvider, \
+                            add_script, add_stylesheet
+
+from autocomplete_provider.api import IStrategyAdapter
 
 class TracEmoji(Component):
     """
     Allows for emoji from https://github.com/arvida/emoji-cheat-sheet.com
     """
-    implements(IWikiSyntaxProvider, ITemplateProvider)
+    implements(IWikiSyntaxProvider, ITemplateProvider, IStrategyAdapter)
 
     HTDOCS_PREFIX = 'trac_emoji'
     EMOJI_DIR = '/icons'
-    EMOJIS = {}
-    STYLE = 'height: 3ex; margin-bottom: -1ex;'
+    STYLE = 'height: 3ex; margin-bottom: -0.5ex;'
 
     def __init__(self, *args, **kwargs):
+        self.emojies = {}
+        self.candidates = []
         icons = os.listdir(''.join([self.htdocs_loc, self.EMOJI_DIR]))
         for icon in icons:
             if icon.endswith('.png'):
-                emoji = ':%s:' %icon[:-4]
-                self.EMOJIS[emoji] = icon
+                self.candidates.append(icon[:-4])
+                emoji = ':%s:' % icon[:-4]
+                self.emojies[emoji] = icon
+        self.candidates.sort()
         super(self.__class__, self).__init__(*args, **kwargs)
+
+    # IStrategyAdapter methods
+
+    def add_strategy(self):
+        return {
+            'id': 'emoji',
+            'match': '/\B:([\-+\w]*)$/',
+            'candidates': self.candidates,
+            'template': """
+function (value) {
+    return '<img src="/trac-plugin-test/chrome/trac_emoji/icons/' + value + '.png" style="height: 3ex; margin-bottom: -0.5ex;" alt=":' + value + ':" title=":' + value + ':" ></img>' + value;
+}
+""",
+            'replace': """
+function (value) {
+    return ':' + value + ': ';
+}
+""",
+            'index': 1
+        }
 
     # IWikiSyntaxProvider methods
 
@@ -40,7 +66,7 @@ class TracEmoji(Component):
         yield (r"(?P<emoji>:[^ ]+:)", create_emoji)
 
     def _format_emoji(self, formatter, emoji):
-        emoji_image = self.EMOJIS.get(emoji)
+        emoji_image = self.emojies.get(emoji)
         if emoji_image is None:
             return emoji
         else:
@@ -53,7 +79,7 @@ class TracEmoji(Component):
 
     # ITemplateProvider methods
     def get_htdocs_loc(self):
-        return pkg_resources.resource_filename('trac_emoji', 'htdocs')
+        return pkg_resources.resource_filename(__name__, 'htdocs')
     htdocs_loc = property(get_htdocs_loc)
 
     def get_htdocs_dirs(self):
